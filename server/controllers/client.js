59dc90507d59adc967163e3bec55ff9bcf4c95da
Ratthatham <ratthatham.siridol@gmail.com>
2023-03-05 00:27:45 +0700

    feat: Already Finished CustomerPage

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,6 @@
 import Product from "../models/product.js"
 import ProductState from "../models/ProductState.js"
+import User from "../models/user.js";
 
 export const getProducts = async(req, res) => {
     try{
@@ -18,6 +19,15 @@ export const getProducts = async(req, res) => {
             })
         )
         res.status(200).json(productWithStat);
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+}
+
+export const getCustomers = async(req, res) => {
+    try{
+        const customers = await User.find({role: "user"})
+        res.status(200).json(customers);
     } catch (error) {
         res.status(404).json({message: error.message})
     }
